da4df9a33eb72c9f65e57010cbc6e391f88e2d0b
Sergey Ivanov <licos.87@mail.ru>
2025-03-29 11:52:10 +0300

    feat: Заканчивает верстку блока 'footer'

--- source/js/main.js
+++ source/js/main.js
@@ -3,3 +3,4 @@ import './utils/juri-swiper.js';
 import './utils/accordion-open.js';
 import './utils/reviews-swiper.js';
 import './utils/feedback-form-validate.js';
+import './utils/play-video.js';
---
+++ source/js/utils/play-video.js
@@ -0,0 +1,3 @@
+const videoBtn = document.querySelector('.about__video-btn');
+const videoFrame = document.querySelector('.about__video');
+const videoContainer = document.querySelector('.about__video-container');
