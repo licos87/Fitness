4b2106683e05419b01a9d2d50149ba9afed8e6c3
Sergey Ivanov <licos.87@mail.ru>
2025-04-08 02:15:48 +0300

    Updates

--- bem-tree.js
+++
@@ -1,6 +0,0 @@
-import bemlinter from 'gulp-html-bemlinter';
-import vfs from 'vinyl-fs';
-
-vfs
-  .src(['./source/**/*.html', '!./source/**/vendor/*.html'])
-  .pipe(bemlinter())
--- source/js/main.js
+++
@@ -1,6 +0,0 @@
-
-import './utils/juri-swiper.js';
-import './utils/accordion-open.js';
-import './utils/reviews-swiper.js';
-import './utils/feedback-form-validate.js';
-import './utils/play-video.js';
